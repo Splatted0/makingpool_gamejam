9094d1efc612d8a2051ec0ffc7e1a1797cc2baa0
_OneSevenFive <sayoon210@gmail.com>
2026-07-04 00:33:02 +0900

    fix(spawner): Monster.SetTarget 시그니처 변경에 맞춰 호출부 수정
    - SetTarget(Core) → SetTarget(Core.GlobalPosition, Core) (Vector2 좌표 + IEntity)
    - HitInfo.Element 확장성 주석: 현재 단일 원소, 2개 조합(콤보) 처리하려면
      List<Elemental>로 바꿔 Monster.Hit 적용부를 순회로 확장해야 할 수 있음

--- magic_sequence/class/HitInfo.cs
+++ magic_sequence/class/HitInfo.cs
@@ -4,5 +4,7 @@ public struct HitInfo
 {
     public int Damage;
     public Team SourceTeam;
+    // 확장성: 지금은 단일 원소. 2개 조합(콤보)을 처리하려면 List<Elemental>로 바꿔야 할 수 있음.
+    // 그 경우 Monster.Hit의 디버프 적용부를 리스트 순회로 확장.
     public Elemental Element;
 }
