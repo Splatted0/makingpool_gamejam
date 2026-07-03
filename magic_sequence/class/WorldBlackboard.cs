4d22890b374433876e48b1592e84970304ca2f48
Splatted0 <junwad1100@gmail.com>
2026-07-03 22:15:17 +0900

    Update WorldBlackboard.cs

--- magic_sequence/class/WorldBlackboard.cs
+++ magic_sequence/class/WorldBlackboard.cs
@@ -2,5 +2,8 @@
 public static class Blackboard
 {
     public static Main Main { private get; set; }
+    public static int Health => Main.Health;
+    public static int Wave => Main.Wave;
+    public static int Gold => Main.Gold;
     public static Node BattleWorld => Main.BattleWorld;
 }
