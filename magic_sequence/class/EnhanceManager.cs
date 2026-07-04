2a1424c65e8d53e9383642191999c2ecdc03b803
Splatted0 <junwad1100@gmail.com>
2026-07-04 15:40:42 +0900

    enhance 수정

--- magic_sequence/class/EnhanceManager.cs
+++ magic_sequence/class/EnhanceManager.cs
@@ -1,21 +1,26 @@
-using Godot;
+﻿using Godot;
 
 public partial class EnhanceManager : CanvasLayer
 {
     [Signal] public delegate void EnhanceEndEventHandler();
-    [Signal] public delegate void InhanceFinishedEventHandler();
-    
+
     [Export] public MagicChangeManager MagicChanceManager;
     [Export] public WandUi GetWandUi;
     [Export] public BaseButton ExitButton;
     [Export] public BaseButton GetWandButton;
+    [Export] public Button MagicRerollButton;
+    [Export] public Button WandRerollButton;
 
     private Wand _getWand;
-
+    private EnhanceData _enhanceData;
+    private int _getMagicCount;
+    
     public override void _Ready()
     {
         ExitButton.Pressed += OnExitButtonPressed;
         GetWandButton.Pressed += OnGetWandButtonPressed;
+        MagicRerollButton.Pressed += OnMagicRerollButtonPressed;
+        WandRerollButton.Pressed += OnWandRerollButtonPressed;
         MagicChanceManager.MagicChangeEnd += OnMagicChangeEnd;
         Setup();
     }
@@ -23,23 +28,44 @@ public partial class EnhanceManager : CanvasLayer
     public void Setup()
     {
         ExitButton.Visible = false;
+        _getMagicCount = 0;
 
+        int idx = Math.Clamp(Blackboard.Wave, 0, Blackboard.EnhanceDataList.Length - 1);
+        _enhanceData = Blackboard.EnhanceDataList[idx];
 
-        Magic[] magics = DropUtil.GetMagicDrops(Blackboard.MagicPool, 2);
+        Magic[] magics = DropUtil.GetMagicDrops(Blackboard.MagicPool, _enhanceData.DropMagicCount);
         MagicChanceManager.Setup(magics);
-        
-        if (Blackboard.Wands.Length >= 3) {return;}
-        Wand[] wands = DropUtil.GetWandDrops(Blackboard.WandPool, 1);
-        _getWand = wands[0].Duplicate() as Wand;
-        GetWandUi.Setup(_getWand);
-        GetWandUi.Visible = true;
-        GetWandButton.Visible = true;
+        MagicChanceManager.Visible = true;
+
+        MagicRerollButton.Text = "$" + _enhanceData.MagicRerollCost;
+        MagicRerollButton.Visible = magics.Length > 0;
+
+        bool showWand = _enhanceData.IsWandDrop && Blackboard.Wands.Length < 3;
+        if (showWand)
+        {
+            Wand[] wands = DropUtil.GetWandDrops(Blackboard.WandPool, 1);
+            _getWand = wands[0].Duplicate() as Wand;
+            GetWandUi.Setup(_getWand);
+        }
+        GetWandUi.Visible = showWand;
+        GetWandButton.Visible = showWand;
+        WandRerollButton.Text = "$" + _enhanceData.WandRerollCost;
+        WandRerollButton.Visible = showWand;
     }
 
     private void OnMagicChangeEnd()
     {
-        if (!MagicChanceManager.IsGetMagicRemain)
+        if (_getMagicCount >= _enhanceData.MustGetMagicCount) return;
+
+        if (_enhanceData.DropMagicCount - MagicChanceManager.GetMagicRemainCount > 0)
+            _getMagicCount++;
+
+        if (_getMagicCount >= _enhanceData.MustGetMagicCount)
+        {
             ExitButton.Visible = true;
+            MagicRerollButton.Visible = false;
+            MagicChanceManager.Visible = false;
+        }
     }
 
     private void OnGetWandButtonPressed()
@@ -47,13 +73,30 @@ public partial class EnhanceManager : CanvasLayer
         Blackboard.Main.AddWand(_getWand);
         GetWandUi.Visible = false;
         GetWandButton.Visible = false;
-        MagicChanceManager.Setup(new Magic[0]);
-        ExitButton.Visible = true;
+        WandRerollButton.Visible = false;
+    }
+
+    private void OnMagicRerollButtonPressed()
+    {
+        if (!Blackboard.TrySpendGold(_enhanceData.MagicRerollCost)) return;
+
+        ExitButton.Visible = false;
+        Magic[] magics = DropUtil.GetMagicDrops(Blackboard.MagicPool, _enhanceData.DropMagicCount);
+        MagicChanceManager.Setup(magics);
+        MagicRerollButton.Visible = magics.Length > 0;
+    }
+
+    private void OnWandRerollButtonPressed()
+    {
+        if (!Blackboard.TrySpendGold(_enhanceData.WandRerollCost)) return;
+
+        Wand[] wands = DropUtil.GetWandDrops(Blackboard.WandPool, 1);
+        _getWand = wands[0].Duplicate() as Wand;
+        GetWandUi.Setup(_getWand);
     }
 
     private void OnExitButtonPressed()
     {
-        EmitSignal(SignalName.InhanceFinished);
         EmitSignal(SignalName.EnhanceEnd);
     }
 }
